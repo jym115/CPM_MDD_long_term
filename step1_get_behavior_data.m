5dbd79a99bddcc83c3dc181cf91d3fe5bc806385
jym115 <47253379+jym115@users.noreply.github.com>
2022-05-23 19:57:11 +0800

    Update step1_get_behavior_data.m

--- step1_get_behavior_data.m
+++ step1_get_behavior_data.m
@@ -4,15 +4,11 @@ DataDir = '/Users/yumeng/Desktop/1/1zhumadian/network/1relapse_matlab/dmn_nbs/11
 SaveFile = '/Users/yumeng/Desktop/1/1zhumadian/network/1relapse_matlab/matlab_paper_final/cpm_TRD.mat';
 %1.get behavior data
 load /Users/yumeng/Desktop/1/1zhumadian/network/1relapse_matlab/dmn_nbs/110mddvs143hc/DMN_remission.mat 'remit_235' 'dmn20210717' 
-remit_235=dmn20210717(177:(176+235),[3,5:7,23,24]);
+remit_235=dmn20210717(177:(176+235),[3:24]);
+
     remit=remit_235(find (remit_235(:,1)>-2),:);
-    id110=1:235;
-    id110=id110(find (remit_235(:,1)>-2));
-    id110=2000+id110;
-    group=remit(:,1);
-    group_remit=group;
-    design=[group_remit,group_remit,remit(:,2:end)];
-    demomdd=design(:,[3:5,7]);
+    group_remit=remit(:,1);%remitter = 1 non-remitter = -1
+    demomdd=remit(:,[3:5,7]); %age gender education FFD,ect...
 save(SaveFile, 'demomdd', 'group_remit', 'dmn20210717')
 
 
